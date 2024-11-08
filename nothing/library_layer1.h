49643c864130a4e1d877fc834794a70b1838092d
Giang Nguyen <giang.nguyentbk@gmail.com>
2024-11-09 01:30:05 +0700

    [NOREF] Remove nothing directory

--- nothing/library_layer1.h
+++
@@ -1,7 +0,0 @@
-#ifndef LIBRARY_LAYER_1_H_
-#define LIBRARY_LAYER_1_H_
-
-int library_layer_1_function(int a, int b);
-
-
-#endif
--- nothing/library_layer2.h
+++
@@ -1,7 +0,0 @@
-#ifndef LIBRARY_LAYER_2_H_
-#define LIBRARY_LAYER_2_H_
-
-int library_layer_2_function(int a, int b);
-
-
-#endif
